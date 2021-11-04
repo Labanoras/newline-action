8cfb4f2e2e0c9be087354eaf9077a3c550535361
Deividas J <lizdeika@gmail.com>
2021-11-04 11:42:29 +0200

    src map

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-/******/ (() => { // webpackBootstrap
+require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
 /***/ 68:
@@ -20671,3 +20671,4 @@ run();
 module.exports = __webpack_exports__;
 /******/ })()
 ;
+//# sourceMappingURL=index.js.map
